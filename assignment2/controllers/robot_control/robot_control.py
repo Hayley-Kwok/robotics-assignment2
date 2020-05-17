a13dbcd3a1963a1e6bd872aecd3b425e3f32bca3
Hayley <wingk1226@gmail.com>
2020-05-18 00:21:40 +0100

    finished task 3a

--- assignment2/controllers/robot_control/robot_control.py
+++ assignment2/controllers/robot_control/robot_control.py
@@ -9,9 +9,9 @@ gpsData=[]
 
 #Distance Sensors (initialize)
 ds = []
-dsNames = ['ds_right', 'ds_left']
+dsNames = ['ds_right', 'ds_left','ds_front']
 
-for i in range(2):
+for i in range(3):
     ds.append(robot.getDistanceSensor(dsNames[i]))
     ds[i].enable(TIME_STEP)
     
@@ -31,23 +31,31 @@ camera0.enable(TIME_STEP)
 gps= robot.getGPS('gps')
 gps.enable(TIME_STEP)
 
-avoidObstacleCounter = 0
-
+leftCounter = 0
+rightCounter = 0
 # feedback loop: step simulation until receiving an exit event
 while robot.step(TIME_STEP) != -1:
-    leftSpeed = 1.0
-    rightSpeed = 1.0
+    leftSpeed = 9.9
+    rightSpeed = 9.9
     
-    if avoidObstacleCounter > 0:
-        avoidObstacleCounter -= 1
-        leftSpeed = 1.0
-        rightSpeed = -0.8
+    if leftCounter > 0:
+        leftCounter -=1
+        leftSpeed = 7.0
+        rightSpeed = -5.0
+    elif rightCounter >0:
+        rightCounter-=1
+        leftSpeed = -5.0
+        rightSpeed = 7.0
     else:  # read sensors
-        for i in range(2):
-            if ds[i].getValue() < 950.0:
-                avoidObstacleCounter = 30
-                gpsData.append(gps.getValues())
-                print(gpsData)     
+        if ds[2].getValue() < 1800.0:
+            gpsData.append(gps.getValues())
+            if ds[0].getValue() > ds[1].getValue():    
+                leftCounter = 12
+                rightCounter = 0
+            else: 
+                leftCounter = 0
+                rightCounter = 12
+                     
     wheels[0].setVelocity(leftSpeed)
     wheels[1].setVelocity(rightSpeed)
     wheels[2].setVelocity(leftSpeed)
