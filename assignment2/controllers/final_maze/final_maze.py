4e73e5c137ecfdb8a14a8d6dda8e1bed95205d9c
hughyuen <hughyuen01@gmail.com>
2020-05-21 21:02:35 +0800

    Add files via upload
    change method for final maze example

--- assignment2/controllers/final_maze/final_maze.py
+++ assignment2/controllers/final_maze/final_maze.py
@@ -1,7 +1,7 @@
+import math
 from controller import Robot,Camera,GPS
 
 TIME_STEP = 64
-
 robot = Robot()
 
 #gps data
@@ -28,59 +28,39 @@ camera0 = robot.getCamera('camera0')
 camera0.enable(TIME_STEP)
 
 
-gps= robot.getGPS('gps')
-gps.enable(TIME_STEP)
+#gps= robot.getGPS('gps')
+#gps.enable(TIME_STEP)
 
 leftCounter = 0
 rightCounter = 0
 backCounter = 0
 # feedback loop: step simulation until receiving an exit event
 while robot.step(TIME_STEP) != -1:
-    print('right', ds[0].getValue())
-    print('left', ds[1].getValue())
-    print('front', ds[2].getValue())
-    leftSpeed = 7
-    rightSpeed = 7
+ 
+    #print('right', ds[0].getValue())
+    #print('left', ds[1].getValue())
+    # print('front', ds[2].getValue())
+
     
-    if backCounter > 0:
-        backCounter -=1
-        leftSpped = -9.9
-        rightSpeed = -9.9
-        
-    elif leftCounter > 0:
-        leftCounter -=1
-        leftSpeed = 5.3
-        rightSpeed = -3.8
-        
-    elif rightCounter >0:
-        rightCounter-=1
-        leftSpeed = -3.8
-        rightSpeed = 5.3
-        
-    else:  # read sensors
-        
-        if ds[2].getValue() < 2000.0:
-            gpsData.append(gps.getValues())
-            if ds[0].getValue() > ds[1].getValue():    
-                leftCounter = 8
-                rightCounter = 0
-                backCounter = 2
-                
-            else: 
-                leftCounter = 0
-                rightCounter = 8
-                backCounter = 2
-                
-        elif ds[0].getValue() < 1400.0:
-            leftCounter = 0
-            rightCounter = 1
-            
-        elif ds[1].getValue() < 1400.0:
-            leftCounter = 1
-            rightCounter = 0
-     
-        
-   
+    
+    if ds[0].getValue()<1300 and ds[1].getValue()<1300:   
+        leftSpeed =2
+        rightSpeed=0
+        print("f")          
+    if ds[2].getValue()<1100:
+        leftSpeed = 2
+        rightSpeed = -10
+    elif ds[1].getValue()<900:
+        leftSpeed = 2.2
+        rightSpeed = -3
+    else:
+        leftSpeed = 1
+        rightSpeed = 10
+    
+    
+    
+    
+    
                                  
     wheels[0].setVelocity(leftSpeed)
     wheels[1].setVelocity(rightSpeed)
